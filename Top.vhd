7b3be95d5c2ff565dadabc58cf5a7bf771f7c4bc
Erik Zachrisson <erik@zachrisson.info>
2016-10-16 12:52:58 +0200

    Hookup reset synchronizer and FFs

--- Top.vhd
+++ Top.vhd
@@ -5,15 +5,46 @@ use ieee.std_logic_unsigned.all;
 
 use work.Types.all;
 
-entity T is 
-	port (
-	AsyncRst : in bit1;
-	Clk      : in bit1;
-	--
-	Rst_N    : out bit1
-	);
+entity Top is
+  port (
+    AsyncRst : in  bit1;
+    Clk      : in  bit1;
+    --
+    Input    : in  bit1;
+    Output   : out word(2-1 downto 0)
+    );
 end entity;
 
-architecture rtl of T is
+architecture rtl of Top is
+  signal Rst_N, Rst : bit1;
 begin
+  ResetSync : entity work.ResetSync
+    port map (
+      AsyncRst => AsyncRst,
+      Clk      => Clk,
+      --
+      Rst_N    => Rst_N
+    );
+
+  RstAssign : Rst <= not Rst_N;
+
+  FF_sync : entity work.FF_w_synch_reset
+    port map (
+      Rst    => Rst,
+      Clk    => Clk,
+      --
+      Input  => Input,
+      Output => Output(0)
+    );
+
+  FF_async : entity work.FF_w_asynch_reset
+    port map (
+      Rst    => Rst,
+      Clk    => Clk,
+      --
+      Input  => Input,
+      Output => Output(1)
+    );
+
+
 end architecture rtl;
