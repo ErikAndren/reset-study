21021cf64b5be4bb3ccdde45f2cf617b86977e5f
Erik Zachrisson <erik@zachrisson.info>
2016-10-16 12:25:08 +0200

    Implment FF with synchronous reset

--- synch-reset.vhd
+++ synch-reset.vhd
@@ -5,15 +5,32 @@ use ieee.std_logic_unsigned.all;
 
 use work.Types.all;
 
-entity T is 
-	port (
-	AsyncRst : in bit1;
-	Clk      : in bit1;
-	--
-	Rst_N    : out bit1
-	);
+entity T is
+        port (
+        AsyncRst : in  bit1;
+        Clk      : in  bit1;
+        --
+        Input    : in  bit1;
+        Output   : out bit1;
+        --
+        Rst_N    : out bit1
+        );
 end entity;
 
 architecture rtl of T is
+  signal FF_D : bit1;
+  signal FF_Q : bit1;
 begin
+  synchreset : process (Clk)
+  begin
+    if (rising_edge(Clk)) then
+      if AsyncRst = '1' then
+        FF_D <= '0';
+      else
+        FF_D <= Input;
+      end if;
+    end if;
+  end process;
+
+  OutputAssign : Output <= FF_D;
 end architecture rtl;
